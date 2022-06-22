593060e4ed88224047e093b3aa4a43d7009d2d7f
schai2009 <schai20100@gmail.com>
2022-06-21 18:20:22 -0700

    added res error message if param is undefined

--- server.js
+++ server.js
@@ -3,10 +3,24 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals');
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
+  return result;
+}
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   console.log(req.query);
   res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
 
 app.listen(PORT, () => {
