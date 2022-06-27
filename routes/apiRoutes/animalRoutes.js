5fe68aa18fc94dfab7b9337f47d351355c1f9e63
schai2009 <schai20100@gmail.com>
2022-06-26 17:01:05 -0700

    added apiRoutes

--- routes/apiRoutes/animalRoutes.js
+++ routes/apiRoutes/animalRoutes.js
@@ -28,7 +28,6 @@ router.post('/animals', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
-  // if any data in req.body is incorrect, send 400 error back
   if (!validateAnimal(req.body)) {
     res.status(400).send('The animal is not properly formatted.');
   } else {
